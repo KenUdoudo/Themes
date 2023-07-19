3f204392f7bb69a1f8fb0b20f5367e3b55aa4d7b
Ken <kendo302003@gmail.com>
2023-07-19 20:50:14 +0100

    p tag

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div>
       <h2>Themeing with Context API</h2>
+      <p>Practicing theming: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid deserunt voluptate aut tempora, optio ut dolor nam odio veritatis, quaerat architecto quos temporibus adipisci libero sint! Quae cum blanditiis reiciendis.</p>
     </div>
   )
 }
