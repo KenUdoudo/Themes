4b0816561b3aa4ebb53d13d31451b0517ded2816
Ken <kendo302003@gmail.com>
2023-07-20 20:32:45 +0100

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ function App() {
       <h2>Themes in React</h2>
       <p>This is ane example of how to make themes in React application. Watch my new video to see  how to do this the easy way.</p>
       <p>Example contains React with Typescript, Context API and React Hooks.</p>
+
+      <div>
+        <button>Light Theme</button>
+        <button>Dark Theme</button>
+      </div>
     </div>
   )
 }
