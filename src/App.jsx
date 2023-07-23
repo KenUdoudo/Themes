138fe0a00e910a4a32d3e680014ba06cff9b03c9
Ken <kendo302003@gmail.com>
2023-07-23 05:09:41 +0100

    flex

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
 
   return (
-    <div>
+    <div className='flex flex-col'>
       <h2>Themes in React</h2>
       <p>This is ane example of how to make themes in React application. Watch my new video to see  how to do this the easy way.</p>
       <p>Example contains React with Typescript, Context API and React Hooks.</p>
