3ccadbdace82e8e2c9b388cd02a37f0da8244f6d
Ken <kendo302003@gmail.com>
2023-07-24 07:06:18 +0100

    font for header

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   
 
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <h2>Themes in React</h2>
+    <div className='flex flex-col justify-center items-center mt-40'>
+      <h2 className="font-semibold text-3xl">Themes in React</h2>
       <p>This is an example of how to make themes in React application. Watch my new video to see  how to do this the easy way.</p>
       <p>Example contains React with Typescript, Context API and React Hooks.</p>
 
