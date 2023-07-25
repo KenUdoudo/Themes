d16b8f7de796dd7ae4b8d313146d5b499e77cb09
Ken <kendo302003@gmail.com>
2023-07-25 20:12:08 +0100

    rounded xl

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <p>Example contains React with Typescript, Context API and React Hooks.</p>
 
       <div className="flex flex-row gap-x-5 mt-4">
-        <button className="border-2 bg-white border-purple-600">Light Theme</button>
+        <button className="border-2 bg-white border-purple-600 rounded-xl">Light Theme</button>
         <button>Dark Theme</button>
       </div>
     </div>
