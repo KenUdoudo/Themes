8a3f952af771f065022f25b49a6ebdd9daddf4e1
Ken <kendo302003@gmail.com>
2023-07-26 05:36:26 +0100

    text color

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='flex flex-col justify-center items-center mt-40'>
       <h2 className="font-semibold text-3xl text-purple-600">Themes in React</h2>
-      <p className="text-xl text-center px-40 mx-20">This is an example of how to make themes in React application. Watch my new video to see  how to do this the easy way.</p>
+      <p className="text-xl text-center px-40 mx-20 text-purple-400">This is an example of how to make themes in React application. Watch my new video to see  how to do this the easy way.</p>
       <p>Example contains React with Typescript, Context API and React Hooks.</p>
 
       <div className="flex flex-row gap-x-5 mt-4">
